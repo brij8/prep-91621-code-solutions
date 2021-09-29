12d395b1e7e0b2406c45c7ded3ef0d37ebf325df
Brighitte Johnson <bmuses@gmail.com>
2021-09-29 12:06:16 -0700

    fix step3, lines 7 & 8

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -4,8 +4,8 @@ const person = {
   hobby: 'h4ck1ng 7h3 p14n3t'
 };
 console.log(person);
-var fullName = 'Crash' + ' Override';
-// var fullName = person.firstName + ' ' + person.lastName;
+// var fullName = 'Crash' + ' Override';
+var fullName = person.firstName + ' ' + person.lastName;
 console.log("The person's name is: ", fullName);
 person.job = '1337 h4x0r';
 console.log("The person's current job is: ", person.job);
