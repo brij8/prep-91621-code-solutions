013efd42258e8aaadd6b913922509ebd1d11ead2
Brighitte Johnson <bmuses@gmail.com>
2021-09-29 13:45:09 -0700

    prep-javascript-functions_func 2 of 9

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -4,3 +4,9 @@ function addTwoNumbers(num1, num2) {
 
 var sum = addTwoNumbers(2, 2);
 console.log('the addTwoNumbers function gives a sum of', sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+var minutes = convertHoursToMinutes(2);
+console.log(minutes);
